dfba3951857dc993a6d11c334342fe30c155436f
Felipe Barreto <felipe.barreto@osf.digital>
2022-11-23 21:58:51 -0300

    initial game with challenge

--- services/game.ts
+++ services/game.ts
@@ -2,8 +2,16 @@ import { prisma } from "./db";
 
 export const createGame = async (option) => {
   const gameMode = await prisma.gameMode.findFirst({ where: { name: option } });
+  const firstChallenge = await prisma.gameChallenge.findFirst({
+    where: { gameModeId: gameMode.id },
+  });
+
   return prisma.game.create({
-    data: { active: true, gameModeId: gameMode.id },
+    data: {
+      active: true,
+      gameModeId: gameMode.id,
+      currentChallengeId: firstChallenge.id,
+    },
     include: { gameMode: true },
   });
 };
